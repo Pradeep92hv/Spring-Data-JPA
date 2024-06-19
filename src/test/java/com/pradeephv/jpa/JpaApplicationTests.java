fd890776f279c8f6f1ce491db37031084abbd0f5
Pradeep  H V <hvpradeep92@gmail.com>
2024-06-19 20:00:47 +0530

    first commit

---
+++ src/test/java/com/pradeephv/jpa/JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pradeephv.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
