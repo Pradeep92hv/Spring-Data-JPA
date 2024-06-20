8289ebbe03b771960008a1c9bfd1eacad46cfcc0
Pradeep  H V <hvpradeep92@gmail.com>
2024-06-20 19:35:29 +0530

    added faker data to author

--- src/main/java/com/pradeephv/jpa/JpaApplication.java
+++ src/main/java/com/pradeephv/jpa/JpaApplication.java
@@ -1,5 +1,6 @@
 package com.pradeephv.jpa;
 
+import com.github.javafaker.Faker;
 import com.pradeephv.jpa.models.Author;
 import com.pradeephv.jpa.models.Video;
 import com.pradeephv.jpa.repositories.AuthorRespository;
@@ -11,6 +12,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class JpaApplication {
@@ -21,12 +24,30 @@ public class JpaApplication {
 		System.out.println("Hello");
 	}
 
-	@Bean
+	//@Bean
 	public CommandLineRunner commandLineRunner(
 			AuthorRespository repository,
 			VideoRespository videoRespository
 			) {
 		return args -> {
+
+			Faker faker=new Faker();
+			List<Author> authors = new ArrayList<>();
+
+			for (int i = 0; i < 50; i++) {
+				Author author = Author.builder()
+						.firstName(faker.name().firstName())
+						.lastName(faker.name().lastName())
+						.age(faker.number().numberBetween(20, 70))
+						.email("contact"+i+"@gmail.com")
+						.build();
+				authors.add(author);
+			}
+
+			repository.saveAll(authors);
+			System.out.println("50 authors added to the database.");
+
+
 			/*var author = Author.builder()
 					.firstName("pradee")
 
@@ -39,12 +60,15 @@ public class JpaApplication {
 
 			 */
 
+			/*
 			var video = Video.builder()
 					.name("abhhhc")
 					.length(5)
 					.build();
 			videoRespository.save(video);
 //			logger.info("Video saved successfully with ID: {}", video.getId());
+
+			 */
 
 		};
 	}
--- src/main/java/com/pradeephv/jpa/repositories/AuthorRespository.java
+++ src/main/java/com/pradeephv/jpa/repositories/AuthorRespository.java
@@ -3,6 +3,19 @@ package com.pradeephv.jpa.repositories;
 import com.pradeephv.jpa.models.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AuthorRespository extends JpaRepository<Author,Integer> {
+import java.util.List;
 
+public interface AuthorRespository extends JpaRepository<Author, Integer> {
+
+    List<Author> findAllByFirstName(String fn);
+
+    List<Author> findAllByFirstNameIgnoreCase(String fn);
+
+    List<Author> findAllByFirstNameContainingIgnoreCase(String fn);
+
+    List<Author> findAllByFirstNameStartingWithIgnoreCase(String fn); // corrected method name
+
+    List<Author> findAllByFirstNameEndsWithIgnoreCase(String fn);
+
+    List<Author> findAllByFirstNameInIgnoreCase(List<String> firstname);
 }
